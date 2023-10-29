40e8dc7a74d4312f868b8de553bdc5a3bd2d1571
eharjes <elias@visiolab.io>
2023-10-29 15:28:42 +0100

    test

--- crawler.py
+++ crawler.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 
 r = requests.get('https://www.uos.de')
 # print(r)
-print(r.status_code)
-print(r.headers)
+# print(r.status_code)
+# print(r.headers)
 # print(r.content)
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.title.text)
-print(soup.text)
+# print(soup.title.text)
+# print(soup.text)
 for l in soup.find_all("a"):
     print(l['href'])
     print(l.text)
