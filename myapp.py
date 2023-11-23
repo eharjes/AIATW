c7645f44e7e714169cb1fda4ea390e05330d0e96
Bluhster <fjaptok@web.de>
2023-11-23 14:12:05 +0100

    added word recommendations

--- myapp.py
+++ myapp.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from Levenshtein import distance
+from english_dictionary.scripts.read_pickle import get_dict
 from searchengine import SearchEngine  # Assuming your search engine code is in search_engine.py
 
 app = Flask(__name__)
@@ -6,11 +8,32 @@ search_engine = SearchEngine('https://vm009.rz.uos.de/crawl/index.html', 4000)
 search_engine.build_index()
 
 history = []
-length = 0
+english_dict = get_dict()
+
+def get_recommendation(query):
+    """Returns closest (Levenshtein distance) word to the given word
+
+    query (string)          = word to get recommendation for
+
+    current_rec (string)    = recommended word (can match the given word)
+    """
+    # initialize recommendation as empty string
+    recommendation = ""
+    # initialize best distance as very high so the first word checked always becomes the first recommendation
+    best_dist = 99999999
+    # look through all words in the english dictionary and track which word has the currently lowest distance to the query
+    for word in english_dict:
+        current_dist = distance(query, word)
+        # if the current distance is smaller than the previously best distance update the recommendation and the best distance
+        if current_dist < best_dist:
+            best_dist = current_dist
+            recommendation = word
+    return recommendation
+
 @app.route('/')
 def home():
 
-    return render_template('home_page_template2.html', history = history, length = length)
+    return render_template('home_page_template2.html', history = history)
 
 @app.route('/search')
 def search():
@@ -19,10 +42,10 @@ def search():
         history.append(query)
     if len(history) > 10:
         history.pop(0)
-    length = len(history)
+    recommendation = get_recommendation(query)
     if query:
         urls = search_engine.search(query.split())
-        return render_template('search_results_template.html', urls=urls, query=query)
+        return render_template('search_results_template.html', urls=urls, query=query, recommendation = recommendation)
     else:
         return '<p>Please enter a search term.</p>', 400
 
